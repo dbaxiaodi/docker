dc64fab9391c077c908fe6c05f84ee3d24c504bc
Jacky Chan <dbaxiaodi@163.com>
2016-09-05 09:04:51 +0800

    Update install.sh
    kernel.sem = 50100 64128000 50100 1280

--- docker_pg/centos-pg/install.sh
+++ docker_pg/centos-pg/install.sh
@@ -29,6 +29,7 @@ net.core.rmem_default = 1048576
 net.core.rmem_max = 4194304
 net.core.wmem_default = 262144
 net.core.wmem_max = 1048576
+kernel.sem = 50100 64128000 50100 1280
 EOF
 
 tee << EOF >> /etc/sudoers
